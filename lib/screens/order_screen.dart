69b02a7e823d8b770dadb84142bdfb3f63b42155
Abhishek Pawar <pawarabhi2004@gmail.com>
2024-05-22 08:30:01 +0530

    Adding Deatil in orderItems

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../provider/order.dart';
+import '../provider/order.dart' show Orders;
+import '../widgets/orderItems.dart';
 
 class OrderScreen extends StatelessWidget {
   const OrderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final OrderData=Provider.of<Orders>(context);
+    final OrderData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(title:Text('Your Order'),
-       ),
-       body: ListView.builder(itemCount: OrderData.orders.length,
-       itemBuilder:(context, index) =>  ),
+      appBar: AppBar(
+        title: Text('Your Order'),
+      ),
+      body: ListView.builder(
+        itemCount: OrderData.orders.length,
+        itemBuilder: (context, index) => OrderItems(OrderData.orders[index]),
+      ),
     );
   }
 }
